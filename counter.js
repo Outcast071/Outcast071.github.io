5f94f52408c94656880dcd9fbee18d0e77ad99e2
Outcast071 <162085722+Outcast071@users.noreply.github.com>
2024-03-15 17:36:08 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,18 +1,40 @@
-let counter=0;
-let number=0;
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('p').innerHTML = localStorage.getItem('counter');
+});
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('p2').innerHTML = localStorage.getItem('number');
+});
+if (!localStorage.getItem('counter')){
+    localStorage.setItem('counter',0);
+}
+if (!localStorage.getItem('number')){
+    localStorage.setItem('number',0);
+}
+
 function count() {
+    let counter= localStorage.getItem('counter');
+    let number = localStorage.getItem('number')
     counter++;
     document.querySelector('p').innerHTML = counter;
+    localStorage.setItem('counter', counter)
     document.querySelector('p2').innerHTML = number;
-    if (counter == 33) {
-    counter = counter - 33
-    number = number + 1
+    if (counter >= 33) {
+    counter= counter - counter
+    number++;
+    localStorage.setItem('number', number);
+    localStorage.setItem('counter', counter);
     }
 }
 
 function reset() {
-    counter= counter - counter;
-    number= number - number;
+    localStorage.setItem('counter', 0);
+    localStorage.setItem('number', 0);
     document.querySelector('p').innerHTML = "0";
     document.querySelector('p2').innerHTML = "0"
 }
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector("#button1").onclick = count;
+});
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector("#button2").onclick = reset;
+});
